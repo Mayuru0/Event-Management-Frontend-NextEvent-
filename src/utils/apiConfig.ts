8f6b5d8d2b91134741813ee79305dbf00bd66089
Mayuru Madhuranga <mayuru@cypsolabs.com>
2025-03-14 14:53:04 +0530

    fix

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,4 +1,4 @@
 
 export const BASE_URL = "event-management-backend-nexteve-production.up.railway.app";
-//export const BASE_URL = "https://event-management-backend-beta.vercel.app/";
+
 //export const BASE_URL="http://localhost:8000";
