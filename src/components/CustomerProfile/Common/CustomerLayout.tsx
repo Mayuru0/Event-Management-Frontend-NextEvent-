68d969d1710b449586a9fd1f0f9a3f510397635e
Mayuru Madhuranga <164411973+Mayuru0@users.noreply.github.com>
2025-03-17 13:02:24 +0530

    Update CustomerLayout.tsx

--- src/components/CustomerProfile/Common/CustomerLayout.tsx
+++ src/components/CustomerProfile/Common/CustomerLayout.tsx
@@ -9,7 +9,7 @@ interface ProfileLayoutProps {
 
 const CustomerLayout: React.FC<ProfileLayoutProps> = ({ children }) => {
   return (
-    <div className="flex flex-col h-screen bg-[#121212]">
+    <div className="flex flex-col  bg-[#121212]">
       <div className="flex flex-col lg:flex-row px-4 lg:px-10 ">
         <CustomerSideBar />
         <main className="flex-1 overflow-y-auto ml-0 lg:ml-1">{children}</main>
