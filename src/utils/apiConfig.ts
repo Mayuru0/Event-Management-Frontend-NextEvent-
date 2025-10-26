a6be7450af2c1d9928eacd09d05c7a9c2103f8f4
Mayuru Madhuranga <mayurumaduranga@gmail.com>
2025-10-26 12:25:42 +0530

    fix api url

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,3 +1,4 @@
 
 //export const BASE_URL = "https://event-management-backend-nexteve-production.up.railway.app";
-export const BASE_URL="http://localhost:8000";
+//export const BASE_URL="http://localhost:8000";
+export const BASE_URL="https://www.nexteventbackend.ooguy.com";
