a709e8b1bb8dddcaee20dceb7a106a6de969b3f0
Mayuru Madhuranga <mayurumaduranga@gmail.com>
2025-11-07 10:31:04 +0530

    set api url

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,4 +1,5 @@
 
 //export const BASE_URL = "https://event-management-backend-nexteve-production.up.railway.app";
 //export const BASE_URL="http://localhost:8000";
-export const BASE_URL="https://www.nexteventbackend.ooguy.com";
+export const BASE_URL1="https://www.nexteventbackend.ooguy.com";
+export const BASE_URL="https://eventmanagementbackend-henna.vercel.app";
