ad0b44ba103edbaa1211dfc51fed10ef0fef4dc9
Mayuru Madhuranga <mayurumaduranga@gmail.com>
2026-01-09 17:47:14 +0530

    fix api

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -2,4 +2,8 @@
 //export const BASE_URL = "https://event-management-backend-nexteve-production.up.railway.app";
 //export const BASE_URL="http://localhost:8000";
 export const BASE_URL1="https://www.nexteventbackend.ooguy.com";
-export const BASE_URL="https://eventmanagementbackend-henna.vercel.app";
+export const BASE_URL="https://event-management-backend-next-event.vercel.app";
+
+
+// const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+// const BASE_URL1 = process.env.NEXT_PUBLIC_BASE_URL1;
